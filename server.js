20b045cb2e7cb11f31b280ed618174454976170a
Blake Bordovsky <bab178@txstate.edu>
2016-09-22 20:12:01 -0500

    #1: -Add password encryption -Verify unique username and password length >= 6 -Give api responses more detail -Add comments to sections

--- server.js
+++ server.js
@@ -4,21 +4,14 @@ var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose       = require('mongoose');
+var bcrypt         = require('bcrypt-nodejs');
 var app            = express();
-var router         = express.Router();
 
 // configuration ===========================================
-
-// config files
-var db = require('./config/db')
 
 // set our port
 var port = process.env.PORT || 4001;
 
-// connect to our mongoDB database
-//mongoose.connect(db.url); 
-
-// get all data/stuff of the body (POST) parameters
 // parse application/json 
 app.use(bodyParser.json()); 
 
@@ -32,16 +25,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /public/img will be /img for users
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/public')); 
-// Mongoose stuff
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+// Mongoose stuff ===========================================
 // connect to our mongoDB database
-mongoose.connect('mongodb://localhost/hammer'); 
-// grab the things we need
-var Schema = mongoose.Schema;
+mongoose.connect('mongodb://localhost/hammer');
 
-// create a schema
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("MongoDb Connected.");
+});
+
+// Schemas ===========================================
+
+var Schema = mongoose.Schema;
 var userSchema = new Schema({
   name: String,
   email: String,
@@ -51,43 +50,125 @@ var userSchema = new Schema({
   imageuri: String
 });
 
-// the schema is useless so far
-// we need to create a model using it
 var User = mongoose.model('User', userSchema);
 
-// make this available to our users in our Node applications
-module.exports = User;
+// API Endpoints ===========================================
 
-app.post('/signup', function(req, res, next) {
-    var new_user = new User ({ 
-    name: req.body.name,
-    email :  req.body.email,
-    username: req.body.username,
-    password: req.body.password,
-    admin: false,
-    imageuri: req.body.imageUri
-    });
-
+app.post('/signup', function(req, res) {
+    
     try
     {
-        new_user.save();
+        // Check if password is good before checking the database
+        if(req.body.password.length >= 6) {
+            // Check if user already exists
+            User.findOne({username: req.body.username}, 'name', function (err, user) {
+                
+                if(user === null)
+                {
+                    var new_user = new User ({ 
+                        name: req.body.name,
+                        email:  req.body.email,
+                        username: req.body.username,
+                        password: encryptPassword(req.body.password),
+                        admin: false,
+                        imageuri: req.body.imageUri
+                    });
+                    
+                    new_user.save();
+                    
+                    var response = {
+                        status  : 200,
+                        success : 'Successfully Signed Up as: ' + req.body.name
+                    }
+                    res.end(JSON.stringify(response));
+                }
+                else 
+                {
+                    var response = {
+                        status  : 500,
+                        error : 'A user already exists with this username.'
+                    }
+                    res.end(JSON.stringify(response));
+                }
+            });
+        }
+        else {
+            var response = {
+                status  : 500,
+                error : 'Password must be at least 6 characters.'
+            }
+            res.end(JSON.stringify(response));
+        }
+        
     }
-    catch(error)
+    catch(err)
     {
-        return res.json("Failed to create new User.", error);
+       console.log(err);
+       var response = {
+            status  : 500,
+            error : 'Fatal error during Sign Up.'
+        }
+        res.end(JSON.stringify(response));
     }
-    
-    return res.json("Successfully created new User.");
-    //res.redirect('/');
 });
 
-app.get('/users', function(req, res, next) {
-    res.send(JSON.stringify(User.find({})));
-});
+app.post('/signin', function(req, res) {
     
+    try
+    {
+        // Check if user exists in DB
+        User.findOne({username: req.body.username}, 'name password', function (err, user) {
+            var isMatch = bcrypt.compareSync(req.body.password, user.password);
+            if(user === null || err || !isMatch)
+            {
+                var response = {
+                    status  : 500,
+                    error : 'Username or password combination not found or was incorrect.'
+                }
+                res.end(JSON.stringify(response));
+            }
+            else {
+                // TODO: Create a session to keep user signed in
+                
+                var response = {
+                    status  : 200,
+                    success : 'Successfully Signed In as: ' + user.name
+                }
+                res.end(JSON.stringify(response));
+            }
+        });
+    }
+    catch(err)
+    {
+        console.log(err);
+        var response = {
+            status  : 500,
+            error : 'Fatal error during Sign In.'
+        }
+        
+        res.end(JSON.stringify(response));
+    }
+});
+
+app.get('/signout', function (req, res) {
+  delete req.session.user_id;
+  res.redirect('/signin');
+});
+
+app.get('/users', function(req, res) {
+    res.end();
+});
+
+// Server Start ===========================================
+
 app.listen(port);
-
 console.log("Server is now listening on port: " + port);
 
-module.exports = router;
 exports = module.exports = app;  
+
+encryptPassword = function(password) {
+    // Generate a salt
+    var salt = bcrypt.genSaltSync(10);
+    // Hash the password with the salt
+    return bcrypt.hashSync(password, salt);
+}
